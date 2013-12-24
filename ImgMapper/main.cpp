25b0239121148bbf31554a5b0ce95f067010eac2
Nicolas LATTUADA <n.lattuada@resto-in.com>
2013-12-24 14:01:08 +0100

    Initial Commit

---
+++ ImgMapper/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ImgMapper
+//
+//  Created by nicolas lattuada on 24/12/13.
+//  Copyright (c) 2013 nicolas lattuada. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
